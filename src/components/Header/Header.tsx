f5ba7a05c8ab7ebf1b6e658e1518a92198472fb3
victor gabriel <deoliveiraabreuvictorgabriel@gmail.com>
2021-11-28 18:45:47 -0300

    adding header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,49 @@
+import Logo from '../../assets/shared/logo.svg'
+import NavBar from '../../assets/shared/icon-hamburger.svg'
+import Close from '../../assets/shared/icon-close.svg'
+import './header.scss'
+import { useState } from 'react'
+import { Link ,useLocation} from 'react-router-dom'
+export const Header = () =>{
+    const [menusState, setMenuState] = useState(false)
+    function handleOpenCloseMenuh (){
+        setMenuState(!menusState)
+    }
+    const locationName = useLocation().pathname
+
+    
+    return(
+        <div className="header_conteiner">
+            <div className="logo">
+                <img src={Logo}alt="Logo" />
+            </div>
+            <div className="navBar">
+                <img src={NavBar} alt="" onClick={handleOpenCloseMenuh} />
+            </div>
+            <hr/>
+            <ul className="menu" style={{display:`${menusState ? 'flex' : 'none'}`}}>
+                    <img  className="close" src={Close} alt="" onClick={handleOpenCloseMenuh}/>
+                    <li className={`${locationName === '/' ? 'active' : '' }`} key="HOME">
+                        <strong>00</strong>
+                        <Link to="/">HOME</Link>
+                        
+                    </li>
+                    <li className={`${locationName === '/destination' ? 'active' : '' }`} key="DESTINATION">
+                        <strong>01</strong>
+                        
+                        <Link to="/destination">DESTINATION</Link>
+                    </li>
+                    <li className={`${locationName === '/crew' ? 'active' : '' }`} key="CREW">
+                        <strong>02</strong>
+                        
+                        <Link to="/crew">CREW</Link>
+                    </li>
+                    <li className={`${locationName === '/technology' ? 'active' : '' }`} key="TECHNOLOGY">
+                        <strong>03</strong>
+                        
+                        <Link to="/technology">TECHNOLOGY</Link>
+                    </li>
+                </ul>
+        </div>
+    )
+}
