532ab72a3a80c500a7b8893ced101502fe961b8c
victor gabriel <deoliveiraabreuvictorgabriel@gmail.com>
2021-12-01 15:08:08 -0300

    starting create crew page

--- src/pages/Crew/index.tsx
+++ src/pages/Crew/index.tsx
@@ -1,9 +1,11 @@
+import { CrewNavBar } from '../../components/CrewNavBar'
 import { Header } from '../../components/Header/Header'
 import './index.scss'
 export const Crew = () =>{
     return (
         <div className="crew_conteiner">
             <Header/>
+            <CrewNavBar/>
         </div>
     )
 }
